44bc9682e40535ca8b1c0f96cc5de951f126ce2c
uzlopak <aras.abbasi@googlemail.com>
2022-11-25 13:59:54 +0100

    mcve for helmet issue

--- test.ts
+++ test.ts
@@ -1,5 +1,7 @@
 import { fastify as Fastify } from "fastify"
+import fastifyHelmet from "@fastify/helmet"
 
 const fastify = Fastify({})
 
+fastify.register(fastifyHelmet)
 fastify.listen({ port: 0 })
